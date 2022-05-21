64a86ab14e60f4a63e0a56ce1e92f083cb43bf31
jitukumarsingh29 <jitukumarsingh29@gmail.com>
2022-05-21 12:41:16 +0530

    changed the app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react from Jitu new testing tech
+          I was changed on the feature branch.
         </a>
       </header>
     </div>
